6c81de5fd7d781bc4693d64b70ecbe25dc3477c7
marufan <26137436+marufan@users.noreply.github.com>
2022-11-19 11:00:46 +0100

    init feature ide project

---
+++ task3-preprocessor/features/Main.java
@@ -0,0 +1,12 @@
+/**
+ * TODO description
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		/*if[Main]*/
+		System.out.println("hello");
+		/*end[Main]*/
+	}
+
+}
---
+++ task3-preprocessor/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * TODO description
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello");
+		
+	}
+
+}
